6283d0d9c15a3b0c1262b60c1190e1dd4765357f
Flavio Galon <flavio.galon@agres.com.br>
2021-08-25 22:03:10 -0300

    Decouples event-service test from accountData.

--- src/services/event-service.test.js
+++ src/services/event-service.test.js
@@ -1,14 +1,24 @@
 const eventService = require('./event-service');
 const accountData = require('../data/account-data');
 
-beforeEach(() => {
-    accountData.dropTable();
-});
-
 it('should create a new account with initial balance', async () => {
     const eventType = 'deposit';
     const transactionData = { "destination":"100", "amount":10 };
-    const transactionResult = await eventService.handleEvent(eventType, transactionData);
+
+    jest.spyOn(accountData, 'getBalance').mockImplementationOnce((accountID) => {
+        return Promise.resolve(undefined);
+    });
+
+    jest.spyOn(accountData, "createAccount").mockImplementationOnce(
+        (accountID, initialBalance) => {
+            return Promise.resolve({
+                id: accountID,
+                balance: initialBalance,
+            });
+        }
+    );
+
+    const transactionResult = await eventService.handleEvent('deposit', transactionData);
 
     expect(transactionResult.destination.id).toEqual(transactionData.destination);
     expect(transactionResult.destination.balance).toEqual(transactionData.amount);
@@ -17,10 +27,26 @@ it('should create a new account with initial balance', async () => {
 it('should increment an account balance', async () => {
     const eventType = 'deposit';
     const transactionData = { "destination":"100", "amount":10 };
+    const initialBalance = 10;
+    const finalBalance = initialBalance + transactionData.amount;
 
-    // Will create the account with balance = 10
-    await eventService.handleEvent(eventType, transactionData);
-    const transactionResult = await eventService.handleEvent(eventType, transactionData);
+    jest.spyOn(accountData, 'getBalance').mockImplementationOnce((accountID) => {
+        return Promise.resolve(initialBalance);
+    });
+
+    jest.spyOn(accountData, "setBalance").mockImplementationOnce(
+        (accountID, newBalance) => {
+            return Promise.resolve({
+                id: accountID,
+                balance: finalBalance,
+            });
+        }
+    );
+
+    const transactionResult = await eventService.handleEvent(
+        eventType,
+        transactionData
+    );
 
     expect(transactionResult.destination.balance).toEqual(20);
 });
