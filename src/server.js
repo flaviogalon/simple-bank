94ec2f885115e54ff88ea3f00e8570009535c0be
Flavio Galon <flavio.galon@agres.com.br>
2021-08-26 22:57:36 -0300

    Removes port magic number and prints a nice and warm live and running message.

--- src/server.js
+++ src/server.js
@@ -6,6 +6,8 @@ const errorHandling = require('./middleware/error-handling');
 
 const app = express();
 
+const PORT = 3000;
+
 // Express built in middleware to parse incoming requests with JSON payload
 app.use(express.json());
 
@@ -16,4 +18,6 @@ app.use('/', resetRoute);
 // Middleware to handle errors
 app.use(errorHandling);
 
-app.listen(3000);
+app.listen(PORT);
+
+console.log(`Live and running on port ${PORT}`);
